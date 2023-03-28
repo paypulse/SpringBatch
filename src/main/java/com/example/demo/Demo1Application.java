03abac2dfdb74208999bc13bf07024b38f9cd403
paypluse <sourcedev5@gmail.com>
2023-03-28 18:23:34 +0900

    project setting for meta data schema

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -3,8 +3,13 @@ package com.example.demo;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.integration.IntegrationDataSourceScriptDatabaseInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.sql.init.DatabaseInitializationSettings;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 //배치 기능 활성화
 @EnableBatchProcessing
@@ -19,5 +24,7 @@ public class Demo1Application extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Demo1Application.class);
 	}
+
+
 
 }
--- src/main/java/com/example/demo/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/demo/job/SimpleJobConfiguration.java
@@ -3,8 +3,12 @@ package com.example.demo.job;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
@@ -15,6 +19,18 @@ public class SimpleJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    @Bean
+    public Job simpleJob(){
+        return jobBuilderFactory.get("simpleJob").start(simpleStep1()).build();
+    }
+
+    @Bean
+    public Step simpleStep1(){
+        return stepBuilderFactory.get("simpleStep1").tasklet((contribution, chunkContext) ->{
+            log.info(">>>>>>>>>> this is first job step 1 ");
+            return RepeatStatus.FINISHED;
+        }).build();
+    }
 
 
 
